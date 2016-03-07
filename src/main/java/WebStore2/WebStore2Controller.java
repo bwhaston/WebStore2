11db765fc2befc27f4b61e2f51f4da833f7e714e
PDFormat <hastonb@students.wwu.edu>
2016-03-06 16:44:49 -0800

    Removed import of AtomicLong from WebStore2Controller.java

--- src/main/java/WebStore2/WebStore2Controller.java
+++ src/main/java/WebStore2/WebStore2Controller.java
@@ -2,7 +2,6 @@ package WebStore2;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
